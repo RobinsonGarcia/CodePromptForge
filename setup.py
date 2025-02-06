c504cf45cc24cc94c01ef222c9d3fa9c0e00b510
Robinson Garcia <robinsongarcia@Robinsons-MacBook-Pro-2.local>
2025-02-06 02:16:43 -0300

    setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codepromptforge",             
-    version="1.0.0",                
+    version="1.0.3",                
     packages=find_packages(),
     include_package_data=True,  # ✅ Include non-Python files like templates and static
 
